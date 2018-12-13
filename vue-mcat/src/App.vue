0960eab437d2c12933595d4d6529f6d703eabed2
Valentin Gavela <valentin.gavela@globant.com>
2018-12-13 16:01:32 -0300

    add Vue project

---
+++ vue-mcat/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <header class="text-center">
+        <img
+          class="gif img-fluid select-none"
+          src="https://raw.githubusercontent.com/valentingavela/mcat/master/dist/img/macricat_animation_OK.gif"
+        >
+      </header>
+      <Banner id="banner"/>
+      <ButtonMiau id="buttonMiau"/>
+      <ButtonsFooter/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Banner from "./components/Banner.vue";
+import ButtonMiau from "./components/ButtonMiau.vue";
+import ButtonsFooter from "./components/ButtonsFooter.vue";
+
+// BOOTSTRAP
+// import BootstrapVue from "bootstrap-vue";
+// Vue.use(BootstrapVue);
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+@Component({
+  components: {
+    Banner,
+    ButtonMiau,
+    ButtonsFooter
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="less">
+@import (css)
+  url("https://fonts.googleapis.com/css?family=Open+Sans:400,700,800");
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Open Sans";
+}
+#app {
+  background: url("assets/1200_fondo_gif.gif");
+  background-repeat: no-repeat;
+  background-position: top center;
+  height: 750px;
+}
+header {
+  height: 184.65px;
+  max-height: 184.65px;
+}
+#banner {
+  height: 154px;
+  max-height: 154px;
+}
+.gif {
+  max-width: 480px;
+  padding-top: 60px;
+}
+
+#buttonMiau {
+  height: 309px;
+  max-height: 309px;
+}
+
+.pointer {
+  cursor: pointer;
+}
+.select-none {
+  user-select: none;
+}
+</style>
---
+++ vue-mcat/src/components/Banner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="text-center">
+    <h1 class="title"><strong class="select-none">FALTAN 500.000 "MIAUS"</strong></h1>
+    <h2 class="subtitle"><strong class="select-none">PARA QUE HAGAMOS SONAR UN MIAUAZO EN LA ROSADA</strong></h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Banner extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+    .title {
+        margin-top: 3px;
+        margin-left: -4px;
+        height: 107.83px;
+        /* TODO: put white */
+        color: white;
+        font-size: 5.2rem;
+        font-weight: 800;
+        letter-spacing: -4.5px;
+    }
+    .subtitle {
+    height: 46px;
+    margin-top: -19px;
+    margin-left: -8px;
+    color: white;
+    /* color: white; */
+    font-weight: bold;
+    letter-spacing: -3.5px;
+    font-size: 38px;
+    }
+
+</style>
